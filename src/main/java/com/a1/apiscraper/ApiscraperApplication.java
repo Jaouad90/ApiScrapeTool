7d5fc860caae688be30ba934309df7c314593ed8
tijn167 <tijnvandenbergh2@hotmail.com>
2018-03-01 12:03:35 +0100

    First

---
+++ src/main/java/com/a1/apiscraper/ApiscraperApplication.java
@@ -0,0 +1,12 @@
+package com.a1.apiscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiscraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiscraperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/a1/apiscraper/ApiscraperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.a1.apiscraper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiscraperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
