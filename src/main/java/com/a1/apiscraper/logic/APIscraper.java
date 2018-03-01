91f77d06792b214cc01807675c448a5ce7e9349f
tijn167 <tijnvandenbergh2@hotmail.com>
2018-03-01 14:28:13 +0100

    Added classes and scraper

---
+++ src/main/java/com/a1/apiscraper/logic/APIscraper.java
@@ -0,0 +1,22 @@
+package com.a1.apiscraper.logic;
+
+import com.a1.apiscraper.domain.API;
+import com.mashape.unirest.http.Unirest;
+
+import java.util.HashMap;
+
+public class APIscraper {
+    private API api;
+
+    public APIscraper(API api) {
+        this.api = api;
+    }
+
+    public void scrape() {} {
+        HashMap<String, String> endpointResults = new HashMap<String, String>();
+       for (String endpoint : api.getEndpoints()) {
+           endpointResults.put(endpoint, Unirest.get(api.getBaseUrl() + endpoint).toString());
+       }
+    }
+
+}
---
+++ src/main/java/com/a1/apiscraper/manager/APIManager.java
@@ -0,0 +1,29 @@
+package com.a1.apiscraper.manager;
+
+import com.a1.apiscraper.domain.API;
+import com.a1.apiscraper.logic.APIscraper;
+import com.a1.apiscraper.repository.APIRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+
+public class APIManager {
+    private ArrayList<API> apiArrayList = new ArrayList<>();
+
+
+    @Autowired
+    private APIRepository apiRepository;
+
+    public APIManager(APIRepository apiRepository) {
+        this.apiRepository = apiRepository;
+        apiArrayList = (ArrayList<API>) apiRepository.findAll();
+    }
+
+    public void doScrape() {
+        for(API api : apiArrayList) {
+            APIscraper tempScraper = new APIscraper(api);
+            tempScraper.scrape();
+
+        }
+    }
+}
