ed4886d97ac3fb9c37e6642dc2756b5637094ea2
tijn167 <tijnvandenbergh2@hotmail.com>
2018-03-05 18:33:03 +0100

    Added overview to homepage, added api and endpoint initializing on launch

--- src/main/java/com/a1/apiscraper/ApiscraperApplication.java
+++ src/main/java/com/a1/apiscraper/ApiscraperApplication.java
@@ -1,7 +1,11 @@
 package com.a1.apiscraper;
 
+import com.a1.apiscraper.domain.API;
+import com.a1.apiscraper.domain.Endpoint;
 import com.a1.apiscraper.domain.Role;
 import com.a1.apiscraper.domain.User;
+import com.a1.apiscraper.repository.APIRepository;
+import com.a1.apiscraper.repository.EndpointRepository;
 import com.a1.apiscraper.repository.RoleRepository;
 import com.a1.apiscraper.repository.UserRepository;
 import com.a1.apiscraper.service.UserService;
@@ -13,6 +17,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,7 +28,10 @@ public class ApiscraperApplication extends SpringBootServletInitializer{
 	private RoleRepository roleRepository;
 	@Autowired
 	private UserService userService;
-
+	@Autowired
+	private EndpointRepository endpointRepository;
+	@Autowired
+	private APIRepository apiRepository;
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(ApiscraperApplication.class);
@@ -33,10 +41,12 @@ public class ApiscraperApplication extends SpringBootServletInitializer{
 	}
 
 	@Bean
+	@Transactional
 	InitializingBean sendDatabase() {
 		return () -> {
 			List<User> userList = new ArrayList<>();
 			List<Role> roleList = new ArrayList<>();
+			List<Endpoint> endpointList = new ArrayList<>();
 			Role role = new Role();
 			role.setName("Admin");
 			User user = new User();
@@ -48,6 +58,18 @@ public class ApiscraperApplication extends SpringBootServletInitializer{
 			role.setUsers(userList);
 			user.setRoles(roleList);
 			userService.save(user);
+			Endpoint endpoint1 = new Endpoint();
+			endpoint1.setName("/currentprice.json");
+			endpointList.add(endpoint1);
+			API api = new API();
+			api.setName("Coindesk API");
+			api.setBaseUrl("https://api.coindesk.com/v1/bpi	");
+			api.setEndpoints(endpointList);
+			endpoint1.setApi(api);
+			apiRepository.save(api);
+			endpointRepository.save(endpoint1);
+
+
 		};
 	}
 }
