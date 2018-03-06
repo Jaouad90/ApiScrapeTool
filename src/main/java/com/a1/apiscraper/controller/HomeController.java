f064fda88526ce554fcfdc694b4dc6353948b608
tijn167 <tijnvandenbergh2@hotmail.com>
2018-03-06 09:08:49 +0100

    Detailview of API added, deleted glyphicons

--- src/main/java/com/a1/apiscraper/controller/HomeController.java
+++ src/main/java/com/a1/apiscraper/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -52,11 +53,6 @@ public class HomeController {
         apiManager.doScrape();
         return new ModelAndView("home/home", "apis", apis);
     }
-    @GetMapping("detail")
-    @Transactional
-    public ModelAndView detail() {
 
-        return new ModelAndView("home/detail");
-    }
 
 }
