54e53dafce21becbfcffa65f661ba5f1cbcf5596
Bob van der Valk <info@mawoo.nl>
2018-03-16 11:35:47 +0100

    Update UserRepository.java

--- src/main/java/com/a1/apiscraper/repository/UserRepository.java
+++ src/main/java/com/a1/apiscraper/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.a1.apiscraper.repository;
 import com.a1.apiscraper.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String userame);
 }
