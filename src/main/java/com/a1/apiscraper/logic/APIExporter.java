77a76fe622d36a393a60db56acd0facda7e9e11f
Jaouad90 <kaouch.j@gmail.com>
2018-03-16 10:51:36 +0100

    resources adjusted and APIExporter added

---
+++ src/main/java/com/a1/apiscraper/logic/APIExporter.java
@@ -0,0 +1,16 @@
+package com.a1.apiscraper.logic;
+
+import com.a1.apiscraper.service.Converter.ConvertService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class APIExporter
+{
+    @Autowired
+    private ConvertService convertService;
+
+    public String convertedData(String data)
+    {
+        return convertService.convertData(data);
+    }
+
+}
