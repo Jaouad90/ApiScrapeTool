786f713246cc0a0276a55e3b5069a80f1dcdb51c
BBouman <Billybouman@ziggo.nl>
2018-03-22 22:09:45 +0100

    Commit

--- src/main/java/com/a1/apiscraper/manager/APIManager.java
+++ src/main/java/com/a1/apiscraper/manager/APIManager.java
@@ -14,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 import java.time.LocalTime;
@@ -29,28 +30,15 @@ public class APIManager {
     @Autowired
     private HyperMediaRepository hyperMediaRepository;
 
-    @org.springframework.transaction.annotation.Transactional
+    @Transactional
     @Scheduled(cron = "0 0/30 * * * ?")
 //    @Scheduled(cron = "0/30 * * * * ?")
-    public void CheckScrape() {
+    public void scrapeTask() {
         ArrayList<API> apiArrayList = (ArrayList<API>) repositoryService.getAllAPIs();
-        ArrayList<API> apiArrayListToScrape = new ArrayList<>();
-        for (API api : apiArrayList) {
-            List<LocalTime> timeList = api.getTimeInterval().getTimeList();
-            int i = 0;
-            for (LocalTime localTime : timeList) {
-                i++;
-                if (i < timeList.size()) {
-                    if (timeList.get(i).isBefore(LocalTime.now().plusMinutes(30)) && timeList.get(i).isAfter(LocalTime.now())) {
-                        apiArrayListToScrape.add(api);
-                    }
-                }
-            }
-        }
-        doScrape(apiArrayListToScrape);
+        doScrape(getApisToScrape(apiArrayList));
     }
 
-    @org.springframework.transaction.annotation.Transactional
+    @Transactional
     public void doScrape(ArrayList<API> apiArrayList) {
         for(API api : apiArrayList) {
             APIScraper tempScraper = new SimpleAPIscraper(api);
@@ -70,5 +58,22 @@ public class APIManager {
                 endpoint.addResult(result);
             }
         }
+    }
+
+    private ArrayList<API> getApisToScrape(ArrayList<API> apis) {
+        ArrayList<API> apiArrayListToScrape = new ArrayList<>();
+        for (API api : apis) {
+            List<LocalTime> timeList = api.getTimeInterval().getTimeList();
+            int i = 0;
+            for (LocalTime localTime : timeList) {
+                i++;
+                if (i < timeList.size()) {
+                    if (timeList.get(i).isBefore(LocalTime.now().plusMinutes(30)) && timeList.get(i).isAfter(LocalTime.now())) {
+                        apiArrayListToScrape.add(api);
+                    }
+                }
+            }
+        }
+        return apiArrayListToScrape;
     }
 }
