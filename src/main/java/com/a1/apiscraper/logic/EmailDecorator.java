ae876ef33bcd92e5a7c3befd2847feff49ea9042
unknown <jhof@avans.nl>
2018-03-23 13:26:29 +0100

    Removed Tijns @

--- src/main/java/com/a1/apiscraper/logic/EmailDecorator.java
+++ src/main/java/com/a1/apiscraper/logic/EmailDecorator.java
@@ -23,7 +23,7 @@ public class EmailDecorator extends DecoratedScraper {
         mailer.setSubject(getApi().getName() + " Has successfully been scraped");
         mailer.setMessage("Successfully scraped API: " + getApi().getName() + " on " + Instant.now().toString() + "."
                 +"\n\nAPIScrapeTool");
-        mailer.setReceivers("tbergh1@student.avans.nl, apiscrapetool@gmail.com");
+        mailer.setReceivers("apiscrapetool@gmail.com");
         mailer.sendMail();
     }
 }
