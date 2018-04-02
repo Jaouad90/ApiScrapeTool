05c5717f72b78496a42d55513336376a0e0d0b9a
BBouman <Billybouman@ziggo.nl>
2018-04-03 00:26:41 +0200

    Added selenium test

--- src/test/java/com/a1/apiscraper/HomeControllerSeleniumTests.java
+++ src/test/java/com/a1/apiscraper/HomeControllerSeleniumTests.java
@@ -1,16 +1,50 @@
 package com.a1.apiscraper;
 
 import com.a1.apiscraper.controller.HomeController;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
+@SpringBootTest(classes = ApiscraperApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("production")
 public class HomeControllerSeleniumTests {
 
+    @LocalServerPort
+    protected int serverPort;
 
+    private static ChromeDriver browser;
+
+    @Before
+    public void openBrowser() {
+        System.setProperty("webdriver.chrome.driver",
+                "src/test/java/com/a1/apiscraper/resources/chromedriver.exe");
+        browser = new ChromeDriver();
+        browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void closeBrowser() {
+        browser.quit();
+    }
+
+    @Test
+    public void failingAuthentication() {
+        String baseUrl = "http://localhost:" + serverPort + "/api/add";
+        browser.get(baseUrl);
+        assertTrue(browser.getTitle().contains("Login"));
+    }
 }
