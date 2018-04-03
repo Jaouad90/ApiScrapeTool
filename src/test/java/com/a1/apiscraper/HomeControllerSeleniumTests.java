53161558b4b33ecd415faaa628568fac3e62bf3c
BBouman <Billybouman@ziggo.nl>
2018-04-03 10:37:53 +0200

    Selenium test added, failingAuthentication

--- src/test/java/com/a1/apiscraper/HomeControllerSeleniumTests.java
+++ src/test/java/com/a1/apiscraper/HomeControllerSeleniumTests.java
@@ -1,10 +1,15 @@
 package com.a1.apiscraper;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -12,6 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
@@ -38,9 +44,34 @@ public class HomeControllerSeleniumTests {
     }
 
     @Test
+    public void authorizeRequestSucces() {
+        String baseUrl = "http://localhost:" + serverPort + "/api/add";
+        browser.get(baseUrl);
+        assertTrue(browser.getTitle().contains("Login"));
+    }
+
+    @Test
     public void failingAuthentication() {
         String baseUrl = "http://localhost:" + serverPort + "/api/add";
         browser.get(baseUrl);
+
+        WebElement usernameInput =  browser.findElementById("username");
+        usernameInput.sendKeys("test-user");
+        WebElement passwordInput =  browser.findElementById("password");
+        passwordInput.sendKeys("123456");
+        WebElement loginButton = browser.findElementById("loginButton");
+
+        loginButton.click();
+
+        WebDriverWait wait = new WebDriverWait(browser, 10);
+        WebElement messageElement = wait.until(
+                ExpectedConditions.presenceOfElementLocated(By.id("loginError"))
+        );
+
+        String message = messageElement.getText();
+        String successMsg = "Invalid username and password.";
+
+        assertEquals(message, successMsg);
         assertTrue(browser.getTitle().contains("Login"));
     }
 }
